5b46bab7079b980ecf5e26e808620c37ab3ba93f
Carlos <cead22@gmail.com>
2012-01-27 21:55:28 -0430

    validations usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,13 +1,17 @@
 class Usuario < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable,  :validatable,
   devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable,:encryptable
+  :recoverable, :rememberable, :trackable,:encryptable
 
-  validates :email, :uniqueness => true
   validates :username, :presence => true, :uniqueness => true
-  validates :password, :length => {:minimum => 5}
-  validates :password, :confirmation => true
+
+  validates_presence_of     :password
+  validates_confirmation_of :password
+  validates :password, :length => {:minimum => 6}
+
+  validates :rol, :inclusion => {:in => %w(admin conector observador)}
+  
   attr_accessible :username, :email, :password_confirmation, :remember_me, :encrypted_password, :password_salt, :rol, :password
 
   has_many :centros
